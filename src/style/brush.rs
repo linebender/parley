7399af22df37e0af35671fa07baa025cebcdc88e
Nico Burns <nico@nicoburns.com>
2024-05-21 15:40:14 +0100

    Implement Skrifa + Tiny-Skia example

--- src/style/brush.rs
+++ src/style/brush.rs
@@ -4,13 +4,4 @@
 /// Trait for types that represent the color of glyphs or decorations.
 pub trait Brush: Clone + PartialEq + Default + core::fmt::Debug {}
 
-/// Empty brush.
-impl Brush for () {}
-
-/// Brush for a 4-byte color value.
-impl Brush for [u8; 4] {}
-
-/// Brush for a 3-byte color value.
-impl Brush for [u8; 3] {}
-
-impl Brush for peniko::Brush {}
+impl<T: Clone + PartialEq + Default + core::fmt::Debug> Brush for T {}
