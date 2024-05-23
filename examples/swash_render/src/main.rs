4b42666be962dcbf0a81128d5c88db20db54b95b
Nico Burns <nico@nicoburns.com>
2024-05-23 23:30:24 +0100

    Move render_glyph back into it's own function

--- examples/swash_render/src/main.rs
+++ examples/swash_render/src/main.rs
@@ -6,15 +6,15 @@
 
 use image::codecs::png::PngEncoder;
 use image::{self, Pixel, Rgba, RgbaImage};
-use parley::layout::{Alignment, GlyphRun, Layout};
+use parley::layout::{Alignment, Glyph, GlyphRun, Layout};
 use parley::style::{FontStack, FontWeight, StyleProperty};
 use parley::{FontContext, LayoutContext};
 use peniko::Color;
 use std::fs::File;
 use swash::scale::image::Content;
-use swash::scale::{Render, ScaleContext, Source, StrikeWith};
+use swash::scale::{Render, ScaleContext, Scaler, Source, StrikeWith};
 use swash::zeno;
-use swash::{FontRef, GlyphId};
+use swash::FontRef;
 use zeno::{Format, Vector};
 
 fn main() {
@@ -133,72 +133,73 @@ fn render_glyph_run(
 
     // Iterates over the glyphs in the GlyphRun
     for glyph in glyph_run.glyphs() {
-        let glyph_id: GlyphId = glyph.id;
-        let glyph_x = run_x + glyph.x;
-        let glyph_y = run_y - glyph.y;
+        let glyph_x = run_x + glyph.x + (padding as f32);
+        let glyph_y = run_y - glyph.y + (padding as f32);
         run_x += glyph.advance;
 
-        // Compute the fractional offset
-        // You'll likely want to quantize this in a real renderer
-        let offset = Vector::new(glyph_x.fract(), glyph_y.fract());
-
-        // Render the glyph using swash
-        let rendered_glyph = Render::new(
-            // Select our source order
-            &[
-                Source::ColorOutline(0),
-                Source::ColorBitmap(StrikeWith::BestFit),
-                Source::Outline,
-            ],
-        )
-        // Select the simple alpha (non-subpixel) format
-        .format(Format::Alpha)
-        // Apply the fractional offset
-        .offset(offset)
-        // Render the image
-        .render(&mut scaler, glyph_id)
-        .unwrap();
-
-        let glyph_width = usize::try_from(rendered_glyph.placement.width).expect("usize < 32 bits");
-        let glyph_height =
-            usize::try_from(rendered_glyph.placement.height).expect("usize < 32 bits");
-        let glyph_origin_x =
-            glyph_x.floor() as i32 + rendered_glyph.placement.left + padding as i32;
-        let glyph_origin_y =
-            (glyph_y.floor() as i32) - rendered_glyph.placement.top + padding as i32;
-
-        match rendered_glyph.content {
-            Content::Mask => {
-                let mut i = 0;
-                for off_y in 0..glyph_height as i32 {
-                    for off_x in 0..glyph_width as i32 {
-                        let x = (glyph_origin_x + off_x) as u32;
-                        let y = (glyph_origin_y + off_y) as u32;
-                        let alpha = rendered_glyph.data[i];
-                        let color = Rgba([color.r, color.g, color.b, alpha]);
-                        img.get_pixel_mut(x, y).blend(&color);
-                        i += 1;
-                    }
-                }
-            }
-            Content::SubpixelMask => unimplemented!(),
-            Content::Color => {
-                for (off_y, row) in rendered_glyph
-                    .data
-                    .chunks_exact(glyph_width * 4)
-                    .enumerate()
-                {
-                    for (off_x, pixel) in row.chunks_exact(4).enumerate() {
-                        let &[r, g, b, a] = pixel else {
-                            panic!("Pixel doesn't have 4 components")
-                        };
-                        let x = (glyph_origin_x + off_x as i32) as u32;
-                        let y = (glyph_origin_y + off_y as i32) as u32;
-                        let color = Rgba([r, g, b, a]);
-                        img.get_pixel_mut(x, y).blend(&color);
-                    }
-                }
-            }
-        };
+        render_glyph(img, &mut scaler, color, glyph, glyph_x, glyph_y);
     }
+}
+
+fn render_glyph(
+    img: &mut RgbaImage,
+    scaler: &mut Scaler,
+    color: Color,
+    glyph: Glyph,
+    glyph_x: f32,
+    glyph_y: f32,
+) {
+    // Compute the fractional offset
+    // You'll likely want to quantize this in a real renderer
+    let offset = Vector::new(glyph_x.fract(), glyph_y.fract());
+
+    // Render the glyph using swash
+    let rendered_glyph = Render::new(
+        // Select our source order
+        &[
+            Source::ColorOutline(0),
+            Source::ColorBitmap(StrikeWith::BestFit),
+            Source::Outline,
+        ],
+    )
+    // Select the simple alpha (non-subpixel) format
+    .format(Format::Alpha)
+    // Apply the fractional offset
+    .offset(offset)
+    // Render the image
+    .render(scaler, glyph.id)
+    .unwrap();
+
+    let glyph_width = rendered_glyph.placement.width;
+    let glyph_height = rendered_glyph.placement.height;
+    let glyph_x = (glyph_x.floor() as i32 + rendered_glyph.placement.left) as u32;
+    let glyph_y = (glyph_y.floor() as i32 - rendered_glyph.placement.top) as u32;
+
+    match rendered_glyph.content {
+        Content::Mask => {
+            let mut i = 0;
+            for pixel_y in 0..glyph_height {
+                for pixel_x in 0..glyph_width {
+                    let x = glyph_x + pixel_x;
+                    let y = glyph_y + pixel_y;
+                    let alpha = rendered_glyph.data[i];
+                    let color = Rgba([color.r, color.g, color.b, alpha]);
+                    img.get_pixel_mut(x, y).blend(&color);
+                    i += 1;
+                }
+            }
+        }
+        Content::SubpixelMask => unimplemented!(),
+        Content::Color => {
+            let row_size = glyph_width as usize * 4;
+            for (pixel_y, row) in rendered_glyph.data.chunks_exact(row_size).enumerate() {
+                for (pixel_x, pixel) in row.chunks_exact(4).enumerate() {
+                    let x = glyph_x + pixel_x as u32;
+                    let y = glyph_y + pixel_y as u32;
+                    let color = Rgba(pixel.try_into().expect("Not RGBA"));
+                    img.get_pixel_mut(x, y).blend(&color);
+                }
+            }
+        }
+    };
 }
