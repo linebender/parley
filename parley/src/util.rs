ee8afc827543190775f54cd450f7d9f15b318566
Nico Burns <nico@nicoburns.com>
2024-07-22 12:58:40 +1200

    Import debug_panic macro from masonry

--- parley/src/util.rs
+++ parley/src/util.rs
@@ -10,3 +10,29 @@ pub fn nearly_eq(x: f32, y: f32) -> bool {
 pub fn nearly_zero(x: f32) -> bool {
     nearly_eq(x, 0.)
 }
+
+/// Panic in debug and `tracing::error` in release mode.
+///
+/// This macro is in some way a combination of `panic` and `debug_assert`,
+/// but it will log the provided message instead of ignoring it in release builds.
+///
+/// It's useful when a backtrace would aid debugging but a crash can be avoided in release.
+#[macro_export]
+macro_rules! debug_panic {
+    () => { ... };
+    ($msg:expr) => {
+        if cfg!(debug_assertions) {
+            panic!($msg);
+        } else {
+            tracing::error!($msg);
+        }
+    };
+    ($msg:expr,) => { debug_panic!($msg) };
+    ($fmt:expr, $($arg:tt)+) => {
+        if cfg!(debug_assertions) {
+            panic!($fmt, $($arg)*);
+        } else {
+            tracing::error!($fmt, $($arg)*);
+        }
+    };
+}
