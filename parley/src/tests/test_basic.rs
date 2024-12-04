0e28f88d054ef1c20fb814de9e1bda41843926fd
Nico Burns <nico@nicoburns.com>
2024-12-04 18:49:04 +1300

    WIP test cases
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- parley/src/tests/test_basic.rs
+++ parley/src/tests/test_basic.rs
@@ -40,3 +40,39 @@ fn placing_inboxes() {
         env.check_snapshot_with_name(test_case_name, &layout);
     }
 }
+
+
+#[test]
+fn only_inboxes_wrap() {
+    let mut env = testenv!();
+
+    let text = "";
+    let mut builder = env.builder(text);
+    for id in 0..10 {
+        builder.push_inline_box(InlineBox {
+            id,
+            index: 0,
+            width: 10.0,
+            height: 10.0,
+        });     
+    }
+    let mut layout = builder.build(text);
+    layout.break_all_lines(Some(40.0));
+    layout.align(None, Alignment::Middle);
+
+    for (i, line) in layout.lines().enumerate() {
+        println!("LINE {i}");
+        for item in line.items() {
+            match item {
+                crate::PositionedLayoutItem::GlyphRun(run) => {
+                    println!("  RUN")
+                },
+                crate::PositionedLayoutItem::InlineBox(ibox) => {
+                    println!("  IBOX {}", ibox.id);
+                },
+            }
+        }
+    }
+
+    env.check_snapshot(&layout);
+}
