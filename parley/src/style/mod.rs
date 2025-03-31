3ba13e12f1af078dcd3ed9faa1e40832929af254
valadaptive <valadaptive@protonmail.com>
2025-03-31 01:47:37 -0400

    impl From<LineHeight> for StyleProperty

--- parley/src/style/mod.rs
+++ parley/src/style/mod.rs
@@ -170,3 +170,9 @@ impl<B: Brush> From<GenericFamily> for StyleProperty<'_, B> {
         StyleProperty::FontStack(f.into())
     }
 }
+
+impl<B: Brush> From<LineHeight> for StyleProperty<'_, B> {
+    fn from(value: LineHeight) -> Self {
+        Self::LineHeight(value)
+    }
+}
