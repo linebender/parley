7363b2023203d97157d35f11364cc0173c2f35f8
Kaur Kuut <strom@nevermore.ee>
2025-07-07 13:56:20 +0300

    Fix `no_std` compilation.

--- parley_core/src/lib.rs
+++ parley_core/src/lib.rs
@@ -13,3 +13,4 @@
 #![cfg_attr(target_pointer_width = "64", warn(clippy::trivially_copy_pass_by_ref))]
 // END LINEBENDER LINT SET
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(not(feature = "std"), no_std)]
