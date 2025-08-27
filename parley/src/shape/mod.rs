41f6ce16e657047c211f65f5d973c06215bbf2e4
Taj Pereira <taj@canva.com>
2025-08-27 18:34:47 +0930

    Use nicer language initialisation

--- parley/src/shape/mod.rs
+++ parley/src/shape/mod.rs
@@ -316,11 +316,7 @@ fn shape_item<'a, B: Brush>(
             harfrust::Direction::LeftToRight
         };
         let script = swash_convert::script_to_harfrust(item.script);
-        let language = if let Some(lang) = item.locale {
-            lang.language().parse::<harfrust::Language>().ok()
-        } else {
-            None
-        };
+        let language = item.locale.and_then(|lang| lang.language().parse::<harfrust::Language>().ok());
         scx.features.clear();
         for feature in rcx.features(item.features).unwrap_or(&[]) {
             scx.features.push(harfrust::Feature::new(
