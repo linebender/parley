871f3e496397563925b1a330edf20259d03e60b8
Nico Burns <nico@nicoburns.com>
2025-09-22 12:44:01 +0100

    Floats WIP
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- parley/src/layout/line/greedy.rs
+++ parley/src/layout/line/greedy.rs
@@ -48,6 +48,25 @@ struct PrevBoundaryState {
     state: LineState,
 }
 
+/// Reason that the line breaker has yielded control flow
+pub enum YieldData {
+    /// Control flow was yielded because a line break was encountered
+    LineBreak(LineBreakData),
+    /// Control flow was yielded because an inline box with `break_on_box` set
+    /// to `true` was encountered
+    InlineBoxBreak(BoxBreakData),
+}
+
+pub struct LineBreakData {
+    pub reason: BreakReason,
+    pub advance: f32,
+    pub line_height: f32,
+}
+
+pub struct BoxBreakData {
+    pub inline_box_id: usize,
+}
+
 #[derive(Clone, Default)]
 struct BreakerState {
     /// The number of items that have been processed (used to revert state)
@@ -140,7 +159,7 @@ impl<'a, B: Brush> BreakLines<'a, B> {
     }
 
     /// Reset state when a line has been committed
-    fn start_new_line(&mut self) -> Option<(f32, f32)> {
+    fn start_new_line(&mut self, reason: BreakReason) -> Option<YieldData> {
         self.state.items = self.lines.line_items.len();
         self.state.lines = self.lines.lines.len();
         self.state.line.x = 0.;
@@ -148,12 +167,16 @@ impl<'a, B: Brush> BreakLines<'a, B> {
         self.state.emergency_boundary = None;
 
         self.finish_line(self.lines.lines.len() - 1);
-        self.last_line_data()
+        Some(YieldData::LineBreak(self.last_line_data(reason)))
     }
 
-    fn last_line_data(&self) -> Option<(f32, f32)> {
+    fn last_line_data(&self, reason: BreakReason) -> LineBreakData {
         let line = self.lines.lines.last().unwrap();
-        Some((line.metrics.advance, line.size()))
+        LineBreakData {
+            reason,
+            advance: line.metrics.advance,
+            line_height: line.size(),
+        }
     }
 
     /// Returns the y-coordinate of the top of the current line
@@ -168,7 +191,13 @@ impl<'a, B: Brush> BreakLines<'a, B> {
 
     /// Computes the next line in the paragraph. Returns the advance and size
     /// (width and height for horizontal layouts) of the line.
-    pub fn break_next(&mut self, max_advance: f32) -> Option<(f32, f32)> {
+    pub fn break_next(&mut self, max_advance: f32) -> Option<YieldData> {
+        self.break_next_line_or_box(max_advance)
+    }
+
+    /// Computes the next line in the paragraph. Returns the advance and size
+    /// (width and height for horizontal layouts) of the line.
+    fn break_next_line_or_box(&mut self, max_advance: f32) -> Option<YieldData> {
         // Maintain iterator state
         if self.done {
             return None;
@@ -241,12 +270,12 @@ impl<'a, B: Brush> BreakLines<'a, B> {
                             self.state.append_inline_box_to_line(next_x);
                             if try_commit_line!(BreakReason::Emergency) {
                                 self.state.item_idx += 1;
-                                return self.start_new_line();
+                                return self.start_new_line(BreakReason::Emergency);
                             }
                         } else {
                             // println!("BOX BREAK");
                             if try_commit_line!(BreakReason::Regular) {
-                                return self.start_new_line();
+                                return self.start_new_line(BreakReason::Regular);
                             }
                         }
                     }
@@ -287,7 +316,7 @@ impl<'a, B: Brush> BreakLines<'a, B> {
                             if try_commit_line!(BreakReason::Explicit) {
                                 // TODO: can this be hoisted out of the conditional?
                                 self.state.cluster_idx += 1;
-                                return self.start_new_line();
+                                return self.start_new_line(BreakReason::Explicit);
                             }
                         } else if
                         // This text can contribute "emergency" line breaks.
@@ -337,7 +366,7 @@ impl<'a, B: Brush> BreakLines<'a, B> {
                                 if try_commit_line!(BreakReason::Regular) {
                                     // TODO: can this be hoisted out of the conditional?
                                     self.state.cluster_idx += 1;
-                                    return self.start_new_line();
+                                    return self.start_new_line(BreakReason::Regular);
                                 }
                             }
                             // Handle the (common) case where we have previously encountered a line-breaking opportunity in the current line
@@ -356,7 +385,7 @@ impl<'a, B: Brush> BreakLines<'a, B> {
                                     self.state.run_idx = prev.run_idx;
                                     self.state.cluster_idx = prev.cluster_idx;
 
-                                    return self.start_new_line();
+                                    return self.start_new_line(BreakReason::Regular);
                                 }
                             }
                             // Otherwise perform an emergency line break
@@ -370,7 +399,7 @@ impl<'a, B: Brush> BreakLines<'a, B> {
                                     self.state.run_idx = prev_emergency.run_idx;
                                     self.state.cluster_idx = prev_emergency.cluster_idx;
 
-                                    return self.start_new_line();
+                                    return self.start_new_line(BreakReason::Emergency);
                                 }
                             } else {
                                 self.state.append_cluster_to_line(next_x);
@@ -389,7 +418,7 @@ impl<'a, B: Brush> BreakLines<'a, B> {
         }
         if try_commit_line!(BreakReason::None) {
             self.done = true;
-            return self.start_new_line();
+            return self.start_new_line(BreakReason::None);
         }
 
         None
