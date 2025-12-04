7c577285926a3b87f18478f294d9aa2b63b035a0
Robert Bastian <4706271+robertbastian@users.noreply.github.com>
2025-12-04 08:29:54 +0100

    Bake composite properties data (#473)
    There is no need for ICU4X's data provider infrastructure, we can
    directly bake the `CodePointTrie<u32>` and access that. This gets around
    the limitation of not being able to bake custom data markers, and
    removes the serde serialization.
    
    #452

--- unicode_data_gen/src/lib.rs
+++ unicode_data_gen/src/lib.rs
@@ -3,7 +3,6 @@
 
 //! See `./main.rs`.
 
-use alloc::{boxed::Box, vec::Vec};
 use icu_codepointtrie_builder::{CodePointTrieBuilder, CodePointTrieBuilderData};
 use icu_collections::codepointtrie::TrieType;
 use icu_locale::LocaleFallbacker;
@@ -14,17 +13,10 @@ use icu_properties::{
         BidiClass, Emoji, ExtendedPictographic, LineBreak, RegionalIndicator, VariationSelector,
     },
 };
-use icu_provider::prelude::icu_locale_core::locale;
-use icu_provider::prelude::*;
-use icu_provider_adapters::fork::ForkByMarkerProvider;
-use icu_provider_export::{
-    DataLocaleFamily, DeduplicationStrategy, ExportDriver, blob_exporter::BlobExporter,
-};
+use icu_provider_export::prelude::*;
 use icu_provider_source::SourceDataProvider;
-
-use unicode_data::*;
-
-extern crate alloc;
+use std::io::{BufWriter, Write};
+use unicode_data::Properties;
 
 const COPYRIGHT_HEADER: &str =
     "// Copyright 2025 the Parley Authors\n// SPDX-License-Identifier: Apache-2.0 OR MIT\n";
@@ -76,96 +68,23 @@ pub fn generate(out: std::path::PathBuf) {
         .unwrap();
     }
 
-    // Generate `CompositePropsV1` data
+    // Generate `CompositeProps` data
     {
-        let custom_source_provider = CompositePropsProvider::new(icu4x_source_provider.clone());
-
-        // The directory for the composite data is `out/composite`.
-        let custom_data_dir = out.clone().join("composite");
-        std::fs::create_dir_all(&custom_data_dir).unwrap();
-        ExportDriver::new(
-            [DataLocaleFamily::single(locale!("en").into())],
-            DeduplicationStrategy::None.into(),
-            LocaleFallbacker::new_without_data(),
-        )
-        .with_markers([CompositePropsV1::INFO])
-        .export(
-            &ForkByMarkerProvider::new(icu4x_source_provider.clone(), custom_source_provider),
-            BlobExporter::new_with_sink(Box::new(
-                std::fs::File::create(custom_data_dir.clone().join("composite.postcard")).unwrap(),
-            )),
-        )
-        .expect("Composite blob export should succeed");
-
-        // Generate a small Rust file to embed the blob bytes
-        std::fs::write(
-        custom_data_dir.clone().join("mod.rs"),
-        COPYRIGHT_HEADER.to_string() + "\n/// Backing data for the `CompositePropsV1` data provider.\npub const COMPOSITE_BLOB: &[u8] = include_bytes!(\"./composite.postcard\");\n",
-    )
-    .unwrap();
-
-        // Write a small mod.rs file in `out` that re-exports the ICU baked data and the composite data.
-        std::fs::write(
-            out.clone().join("mod.rs"),
-            COPYRIGHT_HEADER.to_string() + "\nmod composite;\nmod icu4x_data;\npub use composite::*;\npub use icu4x_data::*;\n",
-        )
-        .unwrap();
-    }
-}
-
-/// A data provider that provides composite properties for a given character.
-#[derive(Debug)]
-pub struct CompositePropsProvider {
-    source: SourceDataProvider,
-}
-
-impl CompositePropsProvider {
-    fn new(source: SourceDataProvider) -> Self {
-        Self { source }
-    }
-}
-
-impl DataProvider<CompositePropsV1> for CompositePropsProvider {
-    fn load(&self, _req: DataRequest<'_>) -> Result<DataResponse<CompositePropsV1>, DataError> {
-        let script_source = CodePointMapData::<Script>::try_new_unstable(&self.source)?;
-        let gc_source = CodePointMapData::<GeneralCategory>::try_new_unstable(&self.source)?;
-        let gcb_source = CodePointMapData::<GraphemeClusterBreak>::try_new_unstable(&self.source)?;
-        let bidi_source = CodePointMapData::<BidiClass>::try_new_unstable(&self.source)?;
-        let emoji_source = CodePointSetData::try_new_unstable::<Emoji>(&self.source).unwrap();
-        let extended_pictographic_source =
-            CodePointSetData::try_new_unstable::<ExtendedPictographic>(&self.source).unwrap();
-        let variation_selector_source =
-            CodePointSetData::try_new_unstable::<VariationSelector>(&self.source).unwrap();
-        let regional_indicator_source =
-            CodePointSetData::try_new_unstable::<RegionalIndicator>(&self.source).unwrap();
-        let linebreak_source =
-            CodePointMapData::<LineBreak>::try_new_unstable(&self.source).unwrap();
-
-        // Load the individual properties from the source provider
-        let script = script_source.as_borrowed();
-        let gc = gc_source.as_borrowed();
-        let gcb = gcb_source.as_borrowed();
-        let bidi = bidi_source.as_borrowed();
-        let emoji = emoji_source.as_borrowed();
-        let variation_selector = variation_selector_source.as_borrowed();
-        let regional_indicator = regional_indicator_source.as_borrowed();
-        let extended_pictographic = extended_pictographic_source.as_borrowed();
-        let linebreak = linebreak_source.as_borrowed();
-
         // Dense values table for 0..=0x10FFFF
         let mut values = Vec::<u32>::with_capacity(0x110000);
         for cp in 0_u32..=0x10FFFF {
             let v = Properties::new(
-                script.get32(cp),
-                gc.get32(cp),
-                gcb.get32(cp),
-                bidi.get32(cp),
-                emoji.contains32(cp) || extended_pictographic.contains32(cp),
-                variation_selector.contains32(cp),
-                regional_indicator.contains32(cp),
+                CodePointMapData::<Script>::new().get32(cp),
+                CodePointMapData::<GeneralCategory>::new().get32(cp),
+                CodePointMapData::<GraphemeClusterBreak>::new().get32(cp),
+                CodePointMapData::<BidiClass>::new().get32(cp),
+                CodePointSetData::new::<Emoji>().contains32(cp)
+                    || CodePointSetData::new::<ExtendedPictographic>().contains32(cp),
+                CodePointSetData::new::<VariationSelector>().contains32(cp),
+                CodePointSetData::new::<RegionalIndicator>().contains32(cp),
                 // See: https://github.com/unicode-org/icu4x/blob/ee5399a77a6b94efb5d4b60678bb458c5eedb25d/components/segmenter/src/line.rs#L338-L351
                 matches!(
-                    linebreak.get32(cp),
+                    CodePointMapData::<LineBreak>::new().get32(cp),
                     LineBreak::MandatoryBreak
                         | LineBreak::CarriageReturn
                         | LineBreak::LineFeed
@@ -183,19 +102,32 @@ impl DataProvider<CompositePropsV1> for CompositePropsProvider {
         }
         .build();
 
-        Ok(DataResponse {
-            metadata: DataResponseMetadata::default(),
-            payload: DataPayload::from_owned(CompositePropsV1Data::new(trie)),
-        })
-    }
-}
+        let mut file =
+            BufWriter::new(std::fs::File::create(out.join("composite").join("mod.rs")).unwrap());
 
-impl IterableDataProvider<CompositePropsV1> for CompositePropsProvider {
-    fn iter_ids(&self) -> Result<alloc::collections::BTreeSet<DataIdentifierCow<'_>>, DataError> {
-        let mut set = alloc::collections::BTreeSet::new();
-        set.insert(DataIdentifierCow::from_locale(DataLocale::default()));
-        Ok(set)
+        writeln!(&mut file, "{COPYRIGHT_HEADER}").unwrap();
+        writeln!(&mut file, "/// Backing data for the `CompositeProps`").unwrap();
+        writeln!(
+            &mut file,
+            "/// Expected size: {}B",
+            size_of_val(&trie) + databake::BakeSize::borrows_size(&trie)
+        )
+        .unwrap();
+        writeln!(&mut file, "#[rustfmt::skip]").unwrap();
+        writeln!(&mut file, "#[allow(unsafe_code, unused_unsafe, clippy::unseparated_literal_suffix, reason = \"databake behaviour\")]").unwrap();
+        writeln!(
+            &mut file,
+            "pub const COMPOSITE: icu_collections::codepointtrie::CodePointTrie<'static, u32> = {};",
+            databake::Bake::bake(&trie, &databake::CrateEnv::default())
+        )
+        .unwrap();
     }
-}
 
-icu_provider::export::make_exportable_provider!(CompositePropsProvider, [CompositePropsV1,]);
+    let mut file = BufWriter::new(std::fs::File::create(out.join("mod.rs")).unwrap());
+
+    writeln!(&mut file, "{COPYRIGHT_HEADER}").unwrap();
+    writeln!(&mut file, "mod composite;").unwrap();
+    writeln!(&mut file, "mod icu4x_data;").unwrap();
+    writeln!(&mut file, "pub use composite::*;").unwrap();
+    writeln!(&mut file, "pub use icu4x_data::*;").unwrap();
+}
