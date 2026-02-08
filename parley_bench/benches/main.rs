4f25d2237c308dc8f602988d5d09e1129a0b0206
valadaptive <79560998+valadaptive@users.noreply.github.com>
2026-02-08 05:24:58 -0500

    Add text-drawing benchmarks (#539)
    Adds a few different benchmark variants, to test the performance impact
    of underline drawing and caching.
    
    For some reason, on my machine (Linux) I need to pass the `-p`
    (parallel) flag to tango when performing a comparison. Without it, the
    "new" code always comes back as ridiculously faster, even when it's not
    been changed at all. I can't find any information on why this would be.

--- parley_bench/benches/main.rs
+++ parley_bench/benches/main.rs
@@ -8,5 +8,18 @@ use tango_bench::{tango_benchmarks, tango_main};
 use parley_bench::benches::{defaults, styled};
 use parley_bench::fontique_benches::system_fonts_init;
 
-tango_benchmarks!(defaults(), styled(), system_fonts_init());
+use parley_bench::draw::{
+    draw_no_underline_cold_cache, draw_no_underline_warm_cache, draw_with_underline_cold_cache,
+    draw_with_underline_warm_cache,
+};
+
+tango_benchmarks!(
+    defaults(),
+    styled(),
+    draw_no_underline_cold_cache(),
+    draw_no_underline_warm_cache(),
+    draw_with_underline_cold_cache(),
+    draw_with_underline_warm_cache(),
+    system_fonts_init()
+);
 tango_main!();
