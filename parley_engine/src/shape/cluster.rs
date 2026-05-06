b0fc1410cf15fde1b29bff39b330d7a156352046
Fangdun Tsai <fundon@pindash.io>
2026-05-07 07:21:37 +0800

    rename to is_emoji_presentation

--- parley_engine/src/shape/cluster.rs
+++ parley_engine/src/shape/cluster.rs
@@ -60,8 +60,8 @@ pub struct Char {
     /// Indexes into the list of styles for the containing text run, to find the style applicable
     /// to this character.
     pub style_index: u16,
-    /// Whether the emoji with non-printing variation selector
-    pub is_emoji_with_non_printing_variation_selector: bool,
+    /// Whether the emoji presentation
+    pub is_emoji_presentation: bool,
 }
 
 /// Whitespace content of a cluster.
@@ -313,11 +313,9 @@ impl CharCluster {
             // variation sequences and
             // <https://www.unicode.org/versions/Unicode17.0.0/core-spec/chapter-23/#G19053> for
             // variation selectors more generally.
-            let is_emoji_with_non_printing_variation_selector =
-                is_emoji_or_pictograph && info.is_variation_selector();
+            let is_emoji_presentation = is_emoji_or_pictograph && info.is_variation_selector();
 
-            let contributes_to_shaping =
-                info.contributes_to_shaping() && !is_emoji_with_non_printing_variation_selector;
+            let contributes_to_shaping = info.contributes_to_shaping() && !is_emoji_presentation;
             if contributes_to_shaping {
                 map_len += 1;
             }
@@ -327,7 +325,7 @@ impl CharCluster {
                 contributes_to_shaping,
                 style_index,
                 is_control_character: info.is_control(),
-                is_emoji_with_non_printing_variation_selector,
+                is_emoji_presentation,
             });
         }
 
@@ -425,7 +423,7 @@ impl<'a> Mapper<'a> {
         let mut mapped = 0;
         for c in self.chars.iter() {
             // If the color emoji has a non-printing variation selector, ignore the variation selector.
-            if c.is_emoji_with_non_printing_variation_selector {
+            if c.is_emoji_presentation {
                 break;
             }
 
