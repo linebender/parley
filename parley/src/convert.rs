5ca6b8abd94cb413918644d2233ac463ce7edff7
Thomas Churchman <thomas@churchman.nl>
2026-07-01 23:02:43 +0200

    Refactor `parley` to use `parley_core::itemizer`

--- parley/src/convert.rs
+++ parley/src/convert.rs
@@ -1,21 +1,6 @@
 // Copyright 2024 the Parley Authors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-use icu_properties::props::Script;
-use parley_core::AnalysisDataSources;
-
-pub(crate) fn script_to_fontique(
-    script: Script,
-    analysis_data_sources: &AnalysisDataSources,
-) -> fontique::Script {
-    analysis_data_sources
-        .script_short_name()
-        .get(script)
-        .unwrap_or("Zzzz")
-        .parse()
-        .unwrap_or(fontique::Script::UNKNOWN)
-}
-
 pub(crate) fn script_to_harfrust(script: fontique::Script) -> harfrust::Script {
     harfrust::Script::from_iso15924_tag(harfrust::Tag::new(&script.to_bytes()))
         .unwrap_or(harfrust::script::UNKNOWN)
