bfe40d08762f4c558913aa7e5159903fafacbe92
Thomas Churchman <thomas@churchman.nl>
2026-08-01 16:26:46 +0200

    Note limitation of `Cluster` in representing graphemes

--- parley/src/layout/cluster.rs
+++ parley/src/layout/cluster.rs
@@ -14,8 +14,11 @@ use parley_engine::{Atom, Glyph, Grapheme, shape::Whitespace};
 
 /// Atomic unit of text.
 ///
-/// This spans a grapheme, which can be multiple characters of source text. See
-/// [UAX #29 § 3][uax-grapheme]. Grapheme edges are caret/selection/hit-testing edges.
+/// This spans a grapheme intersected with a [`Run`], which can be multiple characters of source
+/// text. See [UAX #29 § 3][uax-grapheme]. Grapheme edges are caret/selection/hit-testing edges.
+///
+/// Note a full grapheme as per UAX #29 § 3 can extend past a [`Run`]. In particular, it can extend
+/// past a line boundary and a font boundary. This type does not currently model that.
 ///
 /// [uax-grapheme]: https://www.unicode.org/reports/tr29/#Grapheme_Cluster_Boundaries
 pub struct Cluster<'a, B: Brush> {
