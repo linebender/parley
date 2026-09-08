38b1a170c058b9b5ae92fccdc6163b66756981f3
Tom Churchman <thomas@churchman.nl>
2026-09-08 15:39:35 +0000

    Hang ideographic spaces (#784)
    <!-- Please ensure that you have reviewed our LLM ("AI") policy at
    https://linebender.org/wiki/llm-policy/.
    
    If you did not use any LLM tools, please replace `Unspecified` with
    `None`. -->
    LLM Contributions: Review.
    
    Based on #783.
    
    Follow-up to
    https://github.com/linebender/parley/pull/762#discussion_r3923722770, to
    let ideographic spaces hang (as both Gecko and Blink do that).
    
    Note CSS Text 4 says (effectively) newlines, tabs, and all space
    separators except for non-breaking spaces can hang. From the space
    separators, Gecko and Blink only hang space and ideographic space. This
    now matches that.
    
    <!--
    If our users need to know about this change, please describe that in the
    quote block below.
    What you write here will be edited by us later - it doesn't need to be
    perfect.
    If this change doesn't need a changelog entry, please replace the next
    line with `**Changelog: None**`.
    -->
    **Changelog**
    
    > ## Parley
    > ### Changed
    >
    > - Ideographic spaces can now hang past line ends.

--- parley/src/layout/whitespace.rs
+++ parley/src/layout/whitespace.rs
@@ -11,12 +11,14 @@ use parley_engine::shape::Whitespace;
 ///
 /// [css-hanging]: https://www.w3.org/TR/css-text-4/#white-space-phase-2
 ///
-// Note: CSS Text 4 also includes "other space separators" here, but we don't include them (yet?).
-// See https://github.com/linebender/parley/pull/762#discussion_r3923722770.
+// Note: CSS Text 4 effectively includes tab, newline, plus all of the "space separators" here
+// (Unicode category `Zs`), except for non-breaking space. However, we only include space and
+// ideographic space from the "space separators". See
+// https://github.com/linebender/parley/pull/762#discussion_r3923722770.
 #[inline(always)]
 pub(crate) const fn whitespace_can_hang(whitespace: Whitespace) -> bool {
     matches!(
         whitespace,
-        Whitespace::Space | Whitespace::Tab | Whitespace::Newline
+        Whitespace::Space | Whitespace::IdeographicSpace | Whitespace::Tab | Whitespace::Newline
     )
 }
